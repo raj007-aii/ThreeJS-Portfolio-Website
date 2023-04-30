766ee2ec531ae94eb517b0b23adfbe406b38c6e7
unknown <rajbhuva2003@gmail.com>
2023-04-30 19:55:03 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return(
+    // <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5*index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title}
+          className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        As a React JS developer, i am an expert in building 
+        dynamic and efficient user interfaces using the popular 
+        JavaScript library. I have a deep understanding of the 
+        React component lifecycle, virtual DOM, and JSX syntax, 
+        and i leverage these concepts to create scalable and 
+        maintainable code. I have a passion for developing 
+        responsive and interactive UIs, and i constantly stay 
+        up-to-date with the latest trends and best practices in 
+        the React community. my ability to solve complex problems 
+        and deliver high-quality code makes me a valuable asset to any team.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service}/>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, 'about');
