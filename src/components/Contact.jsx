6e30276c482ad81251aa356ba81bccdd0448bbbb
raj007-aii <80307305+raj007-aii@users.noreply.github.com>
2023-05-01 11:26:45 +0530

    Add files via upload

---
+++ src/components/Contact.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import { useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+import emailjs from '@emailjs/browser'
+import { styles } from '../styles'
+import { EarthCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from '../utils/motion'
+
+
+// template_pm8xa38
+
+// service_owytq2q
+
+// ejPYNU40wAkbF5Nwo
+
+const Contact = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_owytq2q',
+        'template_pm8xa38',
+        {
+          from_name: form.name,
+          to_name: "Raj Bhuva",
+          from_email: form.email,
+          to_email: "rajbhuva2003@gmail.com",
+          message: form.message,
+        },
+        'ejPYNU40wAkbF5Nwo'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
+
+  return (
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div 
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className='flex-[75px] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          // ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input type="text" name="name" value={form.name} onChange={handleChange} placeholder='Enter Name'
+            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your E-mail</span>
+            <input type="email" name="email" value={form.email} onChange={handleChange} placeholder='Enter E-mail'
+            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea rows="7" name="message" value={form.message} onChange={handleChange} placeholder="What's your Message?"
+            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
+          </label>
+
+          <button
+            type='submit'
+            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
+          >
+            {loading ? 'Sending...' : 'Send'}
+
+          </button>
+
+        </form>
+      </motion.div>
+
+      <motion.div 
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+
+      </motion.div>
+    </div>
+  )
+}
+
+export default SectionWrapper(Contact, "contact")
